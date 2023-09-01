c329bd34555be71d867de312367b1cc87dcc1201
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2023-09-01 14:23:13 -0300

    Lampada com quebra a 5 clicks

--- js/lampada.js
+++ js/lampada.js
@@ -1,30 +1,43 @@
-const turnOn = document.getElementById ( 'turnOn' );
-const turnOff = document.getElementById ( 'turnOff' );
-const lamp = document.getElementById ( 'lamp' );
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+const clickLimit = 5; // Defina o número limite de cliques desejado
+let contador = 0;
 
-function isLampBroken () {
-    return lamp.src.indexOf ( 'quebrada' ) > -1
+function isLampBroken() {
+    return lamp.src.indexOf('quebrada') > -1;
 }
 
-function lampOn () {
-    if ( !isLampBroken () ) {
+function lampOn() {
+    if (!isLampBroken()) {
         lamp.src = './img/ligada.jpg';
+        contador += 1;
+        checkClickLimit(); // Verifica o limite de cliques
     }
 }
 
-function lampOff () {
-    if ( !isLampBroken () ) {
+function lampOff() {
+    if (!isLampBroken()) {
         lamp.src = './img/desligada.jpg';
     }
 }
 
-function lampBroken () {
+function lampBroken() {
     lamp.src = './img/quebrada.jpg';
 }
 
-turnOn.addEventListener ( 'click', lampOn );
-turnOff.addEventListener ( 'click', lampOff );
-lamp.addEventListener ( 'mouseover', lampOn );
-lamp.addEventListener ( 'mouseleave', lampOff );
-lamp.addEventListener ( 'dblclick', lampBroken );
+trocaLamp() {
+    lamp.src = '.img/'
+}
 
+function checkClickLimit() {
+    if (contador >= clickLimit) {
+        lampBroken(); // Chama a função para quebrar a lâmpada
+    }
+}
+
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
+//lamp.addEventListener('mouseover', lampOn);
+//lamp.addEventListener('mouseleave', lampOff);
+//lamp.addEventListener('dblclick', lampBroken);
