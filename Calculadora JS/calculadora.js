ac315144efe71128631b9be88cd5340171fe58e4
kaynera1227 <123770284+kaynera1227@users.noreply.github.com>
2023-09-22 13:31:42 -0300

    Add files via upload

--- Calculadora JS/calculadora.js
+++ Calculadora JS/calculadora.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Obtendo elementos HTML
 const display = document.getElementById('display');
 const numeros = document.querySelectorAll('[id*=tecla]');
 const operadores = document.querySelectorAll('[id*=operador]');
@@ -8,8 +9,10 @@ let novoNumero = true;
 let operador;
 let numeroAnterior;
 
+// Função que verifica se há uma operação pendente
 const operacaoPendente = () => operador !== undefined;
 
+// Função que realiza o cálculo
 const calcular = () => {
     if (operacaoPendente()) {
         const numeroAtual = parseFloat(display.textContent.replace('.','').replace(',', '.'));
@@ -19,6 +22,7 @@ const calcular = () => {
     }
 };
 
+// Função que atualiza o display
 const atualizarDisplay = (texto) => {
     if (novoNumero) {
         display.textContent = texto.toLocaleString('BR');
@@ -29,9 +33,11 @@ const atualizarDisplay = (texto) => {
     document.querySelector('#igual').focus();
 };
 
+// Função que insere um número no display
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
 numeros.forEach((numero) => numero.addEventListener('click', inserirNumero));
 
+// Função que seleciona um operador
 const selecionarOperador = (evento) => {
     if (!novoNumero) {
         calcular();
@@ -44,17 +50,20 @@ operadores.forEach((operador) =>
     operador.addEventListener('click', selecionarOperador)
 );
 
+// Função ativada quando o botão de igual é clicado
 const ativarIgual = () => {
     calcular();
     operador = undefined;
 };
 document.getElementById('igual').addEventListener('click', ativarIgual);
 
+// Função que limpa o display
 const limparDisplay = () => (display.textContent = '');
 document
     .getElementById('limparDisplay')
     .addEventListener('click', limparDisplay);
 
+// Função que limpa o cálculo
 const limparCalculo = () => {
     limparDisplay();
     operador = undefined;
@@ -65,20 +74,27 @@ document
     .getElementById('limparCalculo')
     .addEventListener('click', limparCalculo);
 
+// Função que remove o último número do display
 const removerUltimoNumero = () =>
     (display.textContent = display.textContent.slice(0, -1));
 document
     .getElementById('backspace')
     .addEventListener('click', removerUltimoNumero);
 
+// Função que inverte o sinal do número
 const inverterSinal = () => {
     novoNumero = true;
     atualizarDisplay(display.textContent * -1);
 };
 document.getElementById('inverter').addEventListener('click', inverterSinal);
 
+// Função que verifica se há um decimal no display
 const existeDecimal = () => display.textContent.indexOf(',') !== -1;
+
+// Função que verifica se há um valor no display
 const existeValor = () => display.textContent.length > 0;
+
+// Função que insere um decimal no display
 const inserirDecimal = () => {
     if (!existeDecimal()) {
         if (novoNumero) {
@@ -90,6 +106,7 @@ const inserirDecimal = () => {
 };
 document.getElementById('decimal').addEventListener('click', inserirDecimal);
 
+// Mapeamento de teclas para funções
 const mapaTeclado = {
     0: 'numero0',
     1: 'numero1',
@@ -113,6 +130,7 @@ const mapaTeclado = {
     '.': 'decimal',
 };
 
+// Função que mapeia as teclas do teclado físico
 const mapearTeclado = (evento) => {
     const tecla = evento.key;
     const teclaPermitida = () => Object.keys(mapaTeclado).indexOf(tecla) !== -1;
--- timer/timer.js
+++ timer/timer.js
@@ -1,9 +1,10 @@
-var sec=0
-var min=0
-var hr=0
+var sec=0 // Variável para os segundos
+var min=0 // Variável para os minutos
+var hr=0  // Variável para as horas
 
-var interval
+var interval // Variável para armazenar o intervalo (para pausar ou parar)
 
+// Função que formata um dígito para sempre ter dois caracteres (0 à esquerda se necessário)
 function doisDigitos(digito){
     if(digito<10){
         return('0'+digito)
@@ -12,27 +13,28 @@ function doisDigitos(digito){
     }
 }
 
+// Função para iniciar o cronômetro
 function start(){
     watch()
     interval= setInterval(watch,10)
-
-
 }
 
+// Função para pausar o cronômetro
 function pause(){
     clearInterval(interval)
-
 }
 
+// Função para parar o cronômetro e resetar valores
 function stop(){
     clearInterval(interval)
     sec=0
     min=0
+    hr=0
     window.alert("Você parou em: "+document.getElementById('watch').innerText)
     document.getElementById('watch').innerText='00:00:00'
-
 }
 
+// Função para atualizar o cronômetro
 function watch(){
     sec++
     if(sec==60){
@@ -43,5 +45,6 @@ function watch(){
             hr++
         }
     }
+    // Atualiza o elemento HTML com o tempo formatado
     document.getElementById('watch').innerText=doisDigitos(hr)+':'+doisDigitos(min)+':'+doisDigitos(sec)
 }
