2d55cc8fa99a284b5f9c72b958a1d812145d6efc
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2023-09-22 13:27:56 -0300

    Recomentando o código

--- Lampada/js/lampada.js
+++ Lampada/js/lampada.js
@@ -1,58 +1,59 @@
-const turnOn = document.getElementById('turnOn');
-const turnOff = document.getElementById('turnOff');
-const lamp = document.getElementById('lamp');
-const trocar = document.getElementById('trocar');
-const clickLimit = 5; // Defina o número limite de cliques desejado
-let contador = 0;
+// Seleciona os elementos HTML pelo ID e armazena-os em variáveis
+const turnOn = document.getElementById('turnOn'); // Botão de ligar
+const turnOff = document.getElementById('turnOff'); // Botão de desligar
+const lamp = document.getElementById('lamp'); // Imagem da lâmpada
+const trocar = document.getElementById('trocar'); // Botão de trocar lâmpada
+const clickLimit = 5; // Define o número limite de cliques desejado
+let contador = 0; // Inicializa o contador de cliques
 
+// Função para verificar se a lâmpada está quebrada
 function isLampBroken() {
     return lamp.src.indexOf('quebrada') > -1;
 }
 
+// Função para ligar a lâmpada
 function lampOn() {
-    if (!isLampBroken()) {
+    if (!isLampBroken()) { // Verifica se a lâmpada não está quebrada
         lamp.classList.add('luminous'); // Adiciona a classe de efeito de luminosidade
-        lamp.src = './img/ligada.png';
-        contador += 1;
+        lamp.src = './img/ligada.png'; // Altera a imagem para a lâmpada ligada
+        contador += 1; // Incrementa o contador de cliques
         checkClickLimit(); // Verifica o limite de cliques
-        
-    }
-    }
-
-
-function lampOff() {
-    if (!isLampBroken()) {
-        lamp.classList.remove('luminous'); // Remove a classe de efeito de luminosidade
-        lamp.src = './img/desligada.png';
-        
     }
 }
 
+// Função para desligar a lâmpada
+function lampOff() {
+    if (!isLampBroken()) { // Verifica se a lâmpada não está quebrada
+        lamp.classList.remove('luminous'); // Remove a classe de efeito de luminosidade
+        lamp.src = './img/desligada.png'; // Altera a imagem para a lâmpada desligada
+    }
+}
+
+// Função para quebrar a lâmpada
 function lampBroken() {
     lamp.classList.remove('luminous'); // Remove a classe de efeito de luminosidade
-    lamp.src = './img/quebrada.png';
+    lamp.src = './img/quebrada.png'; // Altera a imagem para a lâmpada quebrada
 }
 
+// Função para trocar a lâmpada
 function trocaLamp() {
-    if (isLampBroken()){
+    if (isLampBroken()) { // Verifica se a lâmpada está quebrada
         lamp.classList.remove('luminous'); // Remove a classe de efeito de luminosidade
-        lamp.src = './img/desligada.png';
-        contador = 0;
+        lamp.src = './img/desligada.png'; // Altera a imagem para a lâmpada desligada
+        contador = 0; // Reinicia o contador de cliques
     }
 }
 
+// Função para verificar o limite de cliques
 function checkClickLimit() {
-    if (contador >= clickLimit) {
+    if (contador >= clickLimit) { // Verifica se o contador atingiu o limite
         lamp.classList.remove('luminous'); // Remove a classe de efeito de luminosidade
         lampBroken(); // Chama a função para quebrar a lâmpada
-        
-
     }
 }
 
-turnOn.addEventListener('click', lampOn);
-turnOff.addEventListener('click', lampOff);
-trocar.addEventListener('click', trocaLamp);
-//lamp.addEventListener('mouseover', lampOn);
-//lamp.addEventListener('mouseleave', lampOff);
-//lamp.addEventListener('dblclick', lampBroken);
+// Adiciona eventos de clique aos botões
+turnOn.addEventListener('click', lampOn); // Botão de ligar lâmpada
+turnOff.addEventListener('click', lampOff); // Botão de desligar lâmpada
+trocar.addEventListener('click', trocaLamp); // Botão de trocar lâmpada
+
